3fab9c420fcc0639dee0aa2b30a21ccbba21f794
Matt Kazmar <mkazmar@aviatrix.com>
2022-09-27 09:28:36 -0500

    it helps to actually click replace..

--- main.tf
+++ main.tf
@@ -4,49 +4,49 @@
 resource "local_file" "libvirt_br_wan_xml" {
   for_each = local.host_vms
 
-  content  = chomp(templatefile("${path.module}/br-wan.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/br-wan.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/br-wan.xml"
 }
 
 resource "local_file" "libvirt_br_lan_xml" {
   for_each = local.host_vms
 
-  content  = chomp(templatefile("${path.module}/br-lan.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/br-lan.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/br-lan.xml"
 }
 
 resource "local_file" "libvirt_br_mgmt_xml" {
   for_each = local.host_vms
 
-  content  = chomp(templatefile("${path.module}/br-mgmt.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/br-mgmt.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/br-mgmt.xml"
 }
 
 resource "local_file" "libvirt_vm_xml" {
   for_each = local.host_vms
 
-  content  = chomp(templatefile("${path.module}/vm.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/vm.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/vm.xml"
 }
 
 resource "local_file" "startup_script" {
   for_each = local.host_vms
 
-  content  = chomp(templatefile("${path.module}/startup-sh.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/startup-sh.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/startup.sh"
 }
 
 resource "local_file" "frr_conf" {
   for_each = local.frr_vxlan_confs
 
-  content  = chomp(templatefile("${path.module}/frr_conf.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/frr_conf.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/frr.conf"
 }
 
 resource "local_file" "libvirt_hook_network" {
   for_each = local.frr_vxlan_confs
 
-  content  = chomp(templatefile("${path.module}/libvirt-hook-network.tftpl", each.value), "/r", "")
+  content  = replace(templatefile("${path.module}/libvirt-hook-network.tftpl", each.value), "/r", "")
   filename = "${path.module}/${each.key}/libvirt-hook-network.sh"
 }
 
