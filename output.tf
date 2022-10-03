d26d7300225ac3e770bef678d87618f0d322cfb2
Matt Kazmar <mkazmar@aviatrix.com>
2022-10-03 09:41:46 -0500

    more outputs

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "host_vm_pip" {
   description = "Host and edge VM details."
   value       = google_compute_address.host_vm_pip
 }
+
+output "test_vm_pip" {
+  description = "Host and edge VM details."
+  value       = google_compute_address.test_vm_pip
+}
