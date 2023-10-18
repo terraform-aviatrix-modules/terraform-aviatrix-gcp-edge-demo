f29f45694fa873bd4ce0240fed838839dad7f35b
Solomon Wakhungu <swakhungu@aviatrix.com>
2023-10-17 19:46:21 -0500

    Small update

--- gcpstorage.tf
+++ gcpstorage.tf
@@ -111,7 +111,7 @@ resource "google_storage_bucket_object" "frr_conf" {
 
 # Block this resource from being created if the edge_image_location is set.
 resource "google_storage_bucket_object" "qcow2" {
-  count  = var.edge_image_location == "" ? 0 : 1
+  count  = var.edge_image_location == "" ? 1 : 0
   name   = "edge.qcow2"
   source = var.edge_image_filename
   bucket = google_storage_bucket.bucket.name
